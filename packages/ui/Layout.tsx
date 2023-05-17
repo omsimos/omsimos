60eb7f31d8d57f1729487d54783bdb5238c5d011
Josh Daniel <joshxfi.dev@gmail.com>
2023-05-17 11:20:22 +0800

    chore: update types for layout

--- packages/ui/Layout.tsx
+++ packages/ui/Layout.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import { Navbar } from "./Navbar";
 
-export const Layout = ({ children }: { children: Element | JSX.Element }) => {
+export const Layout = ({
+  children,
+}: {
+  children: React.ReactElement | React.ReactNode;
+}) => {
   return (
     <div className="min-h-screen font-grotesk max-w-screen-lg text-white w-[90%] mx-auto">
       <Navbar />
