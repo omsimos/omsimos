8be50e8101a44ab4c5c2bd5a48ef368a996d4141
Josh Daniel <joshxfi.dev@gmail.com>
2023-05-17 10:16:39 +0800

    fix: wrap children in fragment

--- packages/ui/Layout.tsx
+++ packages/ui/Layout.tsx
@@ -1,15 +1,11 @@
 import * as React from "react";
 import { Navbar } from "./Navbar";
 
-export const Layout = ({
-  children,
-}: {
-  children: JSX.Element | JSX.Element[];
-}) => {
+export const Layout = ({ children }: { children: Element | JSX.Element }) => {
   return (
     <div className="min-h-screen font-grotesk max-w-screen-lg text-white w-[90%] mx-auto">
       <Navbar />
-      {children}
+      <>{children}</>
     </div>
   );
 };
