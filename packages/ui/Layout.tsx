7a623ba37f2dc0ed2ca03eefcb25c6306f9e9357
Josh Daniel <joshxfi.dev@gmail.com>
2023-05-17 12:17:31 +0800

    fix: sync types in ui

--- packages/ui/Layout.tsx
+++ packages/ui/Layout.tsx
@@ -1,11 +1,7 @@
 import * as React from "react";
 import { Navbar } from "./Navbar";
 
-export const Layout = ({
-  children,
-}: {
-  children: React.ReactElement | React.ReactNode;
-}) => {
+export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen font-grotesk max-w-screen-lg text-white w-[90%] mx-auto">
       <Navbar />
