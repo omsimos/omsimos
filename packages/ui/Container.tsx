e8b3ac8b84e2294cc8ea49f92a1a5554efa33404
Dale B <daleban.dev@gmail.com>
2023-06-10 16:27:41 +0800

    chore: add types for container
    Co-authored-by: Josh-Daniel-Bañares <joshxfi.dev@gmail.com>

--- packages/ui/Container.tsx
+++ packages/ui/Container.tsx
@@ -1,13 +1,19 @@
 import * as React from "react";
 
-type Props = {
+type Props<T> = {
   className?: string;
   children: React.ReactNode;
-};
+} & T;
 
-export const Container = ({ className, children }: Props) => {
+export const Container = ({
+  className,
+  children,
+  ...rest
+}: Props<
+  React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+>) => {
   return (
-    <div className={`max-w-screen-xl w-[90%] mx-auto ${className}`}>
+    <div className={`max-w-screen-xl w-[90%] mx-auto ${className}`} {...rest}>
       {children}
     </div>
   );
