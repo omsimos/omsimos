f40c43c8862ed8f6ed13a5e7bc07acd5026640e5
Dale B <daleban.dev@gmail.com>
2023-07-27 22:26:50 +0800

    chore: update tagline

--- apps/freelance/src/pages/index.tsx
+++ apps/freelance/src/pages/index.tsx
@@ -10,12 +10,8 @@ export default function Freelance() {
       <section>
         <Container>
           <h1 className="absolute bg-gradient-to-tr from-[#8c8c8c] to-white bg-clip-text text-2xl tracking-tight text-transparent sm:mt-20 sm:text-3xl  md:mt-52 md:text-4xl lg:text-5xl lg:leading-[1.1]">
-            We transform ideas <br /> into digital
-            <span className="bg-gradient-to-tr from-green-900 to-green-300  bg-clip-text text-transparent ">
-              {" "}
-              —{" "}
-            </span>
-            <span className="font-playfair italic">reality.</span>
+            Weaving dreams <br />
+            into digital <span className="font-playfair italic">reality.</span>
           </h1>
         </Container>
         <Image
