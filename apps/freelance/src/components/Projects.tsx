a57cd8431d04ea59a8f84136766ef5f61d50d646
Dale B <daleban.dev@gmail.com>
2023-08-05 22:29:28 +0800

    chore: featured projects section responsiveness

--- apps/freelance/src/components/Projects.tsx
+++ apps/freelance/src/components/Projects.tsx
@@ -44,35 +44,56 @@ export const Projects = () => {
         </h2>
       </Container>
       <div className="grid xl:grid-cols-3">
-        {projects.map((project, i) => {
-          const { name, description, logo, link } = project;
-
+        {projects.map(({ name, description, logo, link }, i) => {
           return (
             <div
-              className="group relative flex h-[80vh] flex-col justify-between border-r-[1px] border-t-[1px] border-[#3B3B3B] p-20 last-of-type:border-r-0"
+              className="group relative flex min-h-[80vh] flex-col justify-between border-r-[1px] border-t-[1px] border-[#3B3B3B] p-20 last-of-type:border-r-0 md:flex-row lg:gap-10 xl:flex-col xl:gap-0"
               key={name}
             >
-              <span className="text-1xl font-light lg:text-2xl xl:text-3xl 2xl:text-2xl">{`(0${
+              <span className="text-1xl block text-center font-light md:hidden md:text-left lg:text-2xl xl:block">{`(0${
                 i + 1
               }.)`}</span>
-              <p className="font-regular text-2xl transition-opacity duration-700 ease-in-out group-hover:pointer-events-none group-hover:opacity-0 lg:text-3xl xl:text-4xl 2xl:text-5xl">
-                {name}
-              </p>
-              <p className="mx-auto w-3/4 text-xl font-light text-gray-400 transition-opacity duration-700 ease-in-out  group-hover:pointer-events-none group-hover:opacity-0 md:w-2/3 lg:mx-0 lg:w-full lg:text-2xl 2xl:text-2xl">
-                {description}
-              </p>
 
               <Image
                 src={logo}
                 priority
                 quality={100}
-                alt="bg image"
-                className="pointer-events-none absolute left-1/2 top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity  duration-700 ease-in-out group-hover:opacity-100"
+                alt={name}
+                className="pointer-events-none -z-10 object-cover transition-opacity duration-700 ease-in-out group-hover:opacity-100 sm:scale-75 xl:absolute xl:left-1/2 xl:top-1/2 xl:-translate-x-1/2 xl:-translate-y-1/2 xl:opacity-0"
               />
 
+              <p className="font-regular block text-center text-2xl transition-opacity duration-700 ease-in-out sm:text-3xl md:hidden md:text-left xl:block xl:text-4xl xl:group-hover:pointer-events-none xl:group-hover:opacity-0 2xl:text-[2.6rem]">
+                {name}
+              </p>
+              <p className="mx-auto block w-full py-5 text-center text-lg font-light text-gray-400 transition-opacity duration-700 ease-in-out sm:w-2/3 sm:py-8 md:mx-0 md:hidden md:w-2/3 md:py-0 md:text-left lg:mx-0 lg:w-full lg:text-2xl xl:block xl:group-hover:pointer-events-none xl:group-hover:opacity-0 2xl:text-2xl">
+                {description}
+              </p>
+
               <a href={link} target="_blank">
-                <Button name="Visit Website" />
+                <Button
+                  name="Visit Website"
+                  className="mx-auto block md:mx-0 md:hidden xl:flex"
+                />
               </a>
+
+              <div className="hidden flex-col justify-center space-y-20 md:flex xl:hidden">
+                <span className="text-1xl text-center font-light md:text-left lg:text-2xl">{`(0${
+                  i + 1
+                }.)`}</span>
+
+                <div>
+                  <p className="font-regular mb-10 text-left text-3xl">
+                    {name}
+                  </p>
+                  <p className="w-full text-left text-xl font-light text-gray-400 lg:mx-0 lg:w-3/4 lg:text-2xl">
+                    {description}
+                  </p>
+                </div>
+
+                <a href={link} target="_blank">
+                  <Button name="Visit Website" className="mx-auto md:mx-0" />
+                </a>
+              </div>
             </div>
           );
         })}
