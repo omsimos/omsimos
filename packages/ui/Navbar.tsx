94bbfebfea1c8f50e28315355a1c2849f015ed30
Dale B <daleban.dev@gmail.com>
2023-08-23 21:03:43 +0800

    feat: add navbar hover effect

--- packages/ui/Navbar.tsx
+++ packages/ui/Navbar.tsx
@@ -1,15 +1,14 @@
-import * as React from "react";
 import { Container } from "./Container";
 
 export const Navbar = () => {
   return (
-    <Container className="fixed top-0 left-0 right-0 flex justify-between items-center pt-12">
+    <Container className="fixed top-0 left-0 right-0 flex justify-between items-center pt-12 z-40">
       <h1 className="font-grotesk font-light uppercase text-xl ">Omsimos</h1>
 
-      <ul className="md:flex gap-x-24 uppercase text-sm text-gray-300 hidden">
-        <li>Services</li>
-        <li>Projects</li>
-        <li>Contact</li>
+      <ul className="md:flex gap-x-24 uppercase text-base text-gray-300 hidden">
+        <li className="hover-effect">Services</li>
+        <li className="hover-effect">Projects</li>
+        <li className="hover-effect">Contact</li>
       </ul>
     </Container>
   );
