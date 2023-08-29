dcfee402ff76282cd0412372ae01a86c1ab6546f
Josh Daniel <joshxfi.dev@gmail.com>
2023-08-29 15:46:59 +0800

    chore: add link to homepage on navbar

--- packages/ui/Navbar.tsx
+++ packages/ui/Navbar.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import { Container } from "./Container";
 
 export const Navbar = () => {
   return (
     <Container className="flex justify-between items-center pt-12 z-40">
-      <h1 className="font-grotesk font-light uppercase text-xl ">Omsimos</h1>
+      <Link href="/" className="font-grotesk font-light uppercase text-xl ">
+        Omsimos
+      </Link>
 
       <ul className="md:flex gap-x-24 uppercase text-base text-gray-300 hidden">
         <li className="hover-effect">Services</li>
