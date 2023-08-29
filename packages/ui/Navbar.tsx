ec002c8c709454e47bc80d85d5e398be444b2989
Josh Daniel <joshxfi.dev@gmail.com>
2023-08-29 14:55:24 +0800

    chore: remove fix property in navbar

--- packages/ui/Navbar.tsx
+++ packages/ui/Navbar.tsx
@@ -2,7 +2,7 @@ import { Container } from "./Container";
 
 export const Navbar = () => {
   return (
-    <Container className="fixed top-0 left-0 right-0 flex justify-between items-center pt-12 z-40">
+    <Container className="flex justify-between items-center pt-12 z-40">
       <h1 className="font-grotesk font-light uppercase text-xl ">Omsimos</h1>
 
       <ul className="md:flex gap-x-24 uppercase text-base text-gray-300 hidden">
