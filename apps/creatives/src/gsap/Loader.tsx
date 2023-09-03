764272b86f9c793381da31871d418066f6fe18e1
Dale B <daleban.dev@gmail.com>
2023-09-03 16:42:33 +0800

    feat: initial loader timeline animation

--- apps/creatives/src/gsap/Loader.tsx
+++ apps/creatives/src/gsap/Loader.tsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useState } from "react";
 import { gsap } from "gsap";
-import Image from "next/image";
 import useLayoutEffect from "~/hooks/useIsomorphicLayoutEffect";
 
 export const Loader = () => {
@@ -12,41 +11,109 @@ export const Loader = () => {
 
   useLayoutEffect(() => {
     const tl: GSAPTimeline = gsap.timeline({
-      delay: 1.5,
+      delay: 1,
       ease: "power3.inOut",
     });
 
     if (!unmountLoader) {
-      tl.to(
-        "body",
+      tl.fromTo(
+        "#main-text span",
         {
-          overflowY: "auto",
-          onComplete: () => handleUnmountLoader(),
+          opacity: 0,
         },
-        ">"
-      );
+        {
+          opacity: 1,
+          duration: 2.5,
+          stagger: 0.2,
+          ease: "power3.inOut",
+        }
+      )
+        .to("#blocks-1", {
+          height: 0,
+          duration: 1.5,
+          ease: "power3.inOut",
+        })
+        .to(
+          "#blocks-2",
+          {
+            y: "100%",
+            duration: 1.5,
+            ease: "power3.inOut",
+          },
+          "<"
+        )
+        .to(
+          "#blocks-1-trail div",
+          {
+            height: 0,
+            stagger: 0.1,
+            duration: 0.8,
+            ease: "power3.inOut",
+            delay: 0.4,
+          },
+          "<"
+        )
+        .to(
+          "#blocks-2-trail div",
+          {
+            y: "100%",
+            stagger: -0.1,
+            duration: 0.8,
+            ease: "power3.inOut",
+          },
+          "<"
+        )
+
+        .to(
+          "body",
+          {
+            overflowY: "auto",
+            onComplete: () => handleUnmountLoader(),
+          },
+          ">"
+        );
     }
   }, [handleUnmountLoader, unmountLoader]);
 
   return (
     <>
-      {/* {!unmountLoader && ( */}
-      <div className="absolute left-0 top-0 z-50 flex h-screen w-screen flex-col">
-        <div className="flex h-full [&>*]:w-full [&>*]:border [&>*]:bg-black">
-          <div />
-          <div />
-          <div />
-          <div />
-        </div>
+      {!unmountLoader && (
+        <div className="absolute left-0 top-0 z-40 flex h-screen w-screen flex-col">
+          <div className="relative h-full">
+            <div
+              id="blocks-1-trail"
+              className="absolute left-0 right-0 top-0 flex h-full [&>*]:w-full [&>*]:bg-[#00FF66]"
+            >
+              <div />
+              <div />
+              <div />
+              <div />
+            </div>
 
-        <div className="flex h-full [&>*]:w-full [&>*]:border [&>*]:bg-black">
-          <div />
-          <div />
-          <div />
-          <div />
+            <div
+              id="blocks-1"
+              className="absolute left-0 right-0 top-0 flex h-full w-full bg-black"
+            />
+          </div>
+
+          <div className="relative h-full">
+            <div
+              id="blocks-2-trail"
+              className="absolute left-0 right-0 top-0 flex h-full [&>*]:w-full [&>*]:bg-[#00FF66]"
+            >
+              <div />
+              <div />
+              <div />
+              <div />
+            </div>
+
+            <div
+              id="blocks-2"
+              className="absolute left-0 right-0 top-0 flex h-full w-full bg-black"
+            />
+          </div>
         </div>
-      </div>
-      {/* )} */}
+      )}
     </>
   );
 };
--- apps/creatives/src/pages/index.tsx
+++ apps/creatives/src/pages/index.tsx
@@ -12,9 +12,12 @@ export default function Freelance() {
       <main>
         <section>
           <Container>
-            <h1 className="mt-40 bg-gradient-to-tr from-[#8c8c8c] to-white bg-clip-text text-2xl tracking-tight text-transparent sm:text-3xl md:mt-52 md:text-4xl lg:text-5xl lg:leading-[1.1]">
-              Weaving dreams <br />
-              into digital{" "}
+            <h1
+              id="main-text"
+              className="relative z-50 mt-40 bg-gradient-to-tr from-[#8c8c8c] to-white bg-clip-text text-2xl tracking-tight text-transparent sm:text-3xl md:mt-52 md:text-4xl lg:text-5xl lg:leading-[1.1]"
+            >
+              <span>Weaving</span> <span>dreams</span> <br />
+              <span>into</span> <span>digital</span>{" "}
               <span className="font-playfair italic">reality.</span>
             </h1>
           </Container>
