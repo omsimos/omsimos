9b0b18148e1a145c6f352946d954aba6e9bb0fd8
Josh Daniel <joshxfi.dev@gmail.com>
2024-09-20 10:18:08 +0800

    chore: add redirect url to discord

--- apps/creatives/next.config.mjs
+++ apps/creatives/next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@omsimos/ui, @omsimos/configs"],
+
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/8ve2mjJrs7",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
